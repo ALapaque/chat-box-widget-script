22cb384011683b15053cc29f76055c485e914a23
alapaque1 <alapaque@easyorderapp.com>
2024-06-24 09:22:03 +0200

    initial commit

--- src/components/Chatbox/Chatbox.js
+++ src/components/Chatbox/Chatbox.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Chatbox.css';
 
 const Chatbox = () => {
+  useEffect(() => {
+    console.log('Chatbox > widget parameters', {
+      entityId: window.entityId,
+      token: window.restoBookingToken
+    });
+  }, []);
+
   return (
-    <div id="chatbox-container">
-      <div id="chatbox-header">Chat with us!</div>
-      <div id="chatbox-body">
+    <div id='chatbox-container'>
+      <div id='chatbox-header'>Chat with us!</div>
+      <div id='chatbox-body'>
         <p>Welcome to our chat service. How can we help you today?</p>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Chatbox from './components/Chatbox/Chatbox';
+import {createRoot} from 'react-dom/client'
 
 const WIDGET_ID = 'react-chatbox-widget__container'
 // Dynamically create a container for the chatbox
-const container = document.createElement('div');
-container.id = WIDGET_ID;
-document.body.appendChild(container);
+const container = document.getElementById(WIDGET_ID)
 
-// Render the React component into the container
-ReactDOM.render(<Chatbox/>, document.getElementById(WIDGET_ID));
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Chatbox/>);
